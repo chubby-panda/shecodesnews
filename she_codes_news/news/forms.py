46c5a809107432118f4a783f3de80cde81399c43
Sophie Phoon <sophie.j.phoon@gmail.com>
2020-08-01 12:45:06 +0800

    Ordered stories by date; styled form

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -14,6 +14,39 @@ class StoryForm(ModelForm):
                     'placeholder': 'Select a date',
                     'type': 'date'
                 }
-            )
+            ),
+            'title': forms.TextInput (
+                attrs={
+                    'class' : 'form-title'
+                }
+            ),
+            'author': forms.TextInput (
+                attrs={
+                    'class' : 'form-author'
+                }
+            ),
+            'pub_date': forms.TextInput (
+                attrs={
+                    'class' : 'form-pub_date'
+                }
+            ),
+            'content': forms.Textarea (
+                attrs={
+                    'class' : 'form-content'
+                }
+            ),
         }
 
+            # 'category': forms.ChoiceField (
+            #     choices = [
+            #         ('review', 'Review'),
+            #         ('lifestyle', 'Lifestyle'),
+            #         ('domestic', 'Domestic News'),
+            #         ('international', 'International News')
+            #     ],
+            #     attrs = {
+            #         'class': 'form-control',
+            #         'placeholder': 'Select a category',
+            #         'type': 'select',
+            #     }
+            # )
--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -6,3 +6,6 @@ class NewsStory(models.Model):
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
+
+    def __str__(self):
+        return NewsStory.title
